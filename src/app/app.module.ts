e8ae53513920b7d767a178c2d97d9279d3f55bdb
Dunlap, Colleen <colleen.dunlap@marquette.edu>
2019-03-05 15:52:31 -0600

    created nav bar pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { AboutComponent } from './about/about.component';
+import { LegalComponent } from './legal/legal.component';
+import { ConnectComponent } from './connect/connect.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    AboutComponent,
+    LegalComponent,
+    ConnectComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
