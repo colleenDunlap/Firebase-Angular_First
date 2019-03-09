ec2ec5f60034b719a63180dc2d33b8455940fb38
Dunlap, Colleen <colleen.dunlap@marquette.edu>
2019-03-08 18:33:49 -0600

    going to revert to "Routing Module Works Now"
    This reverts commit 5696cd6d323deaa3fc37fb501da8a51bb12ed9d4.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms';
-import{Routes, RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -11,16 +9,6 @@ import { ConnectComponent } from './connect/connect.component';
 import { ContactComponent } from './contact/contact.component';
 import { AngularFireModule } from 'angularfire2';
 import { environment } from '../environments/environment';
-import { HomeComponent } from './home/home.component';
-import { AlertsComponent } from './alerts/alerts.component';
-
-const appRoutes: Routes=[
-  {path: '', component: HomeComponent},
-  {path: 'contact', component: ContactComponent},
-  {path:'legal', component: LegalComponent},
-  {path:'connect', component:ConnectComponent},
-  {path:'alerts', component:AlertsComponent}
-]
 @NgModule({
   declarations: [
     AppComponent,
@@ -28,13 +16,10 @@ const appRoutes: Routes=[
     AboutComponent,
     LegalComponent,
     ConnectComponent,
-    ContactComponent,
-    HomeComponent,
-    AlertsComponent
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes),
 	AngularFireModule.initializeApp(environment.firebase)
   ],
   providers: [],
